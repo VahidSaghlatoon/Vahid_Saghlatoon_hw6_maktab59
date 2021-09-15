80ed331561046f5386e3dcf85fadffa8f77f89c9
vahid <vahid.d.w@gmail.com>
2021-09-15 19:02:14 +0430

    create address class

--- src/com/vahidsaghlatoon/Customers.java
+++ src/com/vahidsaghlatoon/Customers.java
@@ -1,4 +1,11 @@
 package com.vahidsaghlatoon;
 
 public class Customers {
+    private int nationalCode ;
+    private String firstName ;
+    private  String lastName;
+    private int phone ;
+    private String email ;
+
+
 }
